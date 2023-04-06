8b2f8f8d4354196ac460041d18aea2bc178f509c
Julian So <ch.julian.so@gmail.com>
2023-04-05 20:23:30 -0400

    init commit. set up all config files

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
